f7a741b2ffc44736f18208db9585f862fee245db
Jedi Floreta <jedi.floreta@symph.co>
2023-01-18 01:38:31 +0800

    [Feature][Jedi] Attempted to add instruction interface

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,42 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
+    const [instructInput, setInstructInput] = useState("");
     const [phraseInput, setPhraseInput] = useState("");
-    const [result, setResult] = useState();
+    const [phraseButton, setPhraseButton] = useState(false);
+    const [editResult, setEditResult] = useState();
+    const [instructResult, setInstructResult] = useState();
 
-    async function onSubmit(event) {
+    async function onSubmitInstruction(event) {
         event.preventDefault();
         try {
+            // console.log(`Inside onSubmitInstruction: ${phraseButton}`);
+            setPhraseButton(true);
+            // console.log(`Inside onSubmitInstruction2: ${phraseButton}`);
+            setInstructResult(`${instructInput}`);
+            setInstructInput("");
+        } catch (error) {
+            console.error(error);
+            alert(error.message);
+        }
+    }
+    async function onSubmitPhrase(event) {
+        event.preventDefault();
+        try {
+            // if (!phraseButton) {
+            //     throw (
+            //         new Error(`Please provide a valid instruction first`),
+            //     );
+            // }
             const response = await fetch("/api/edit_try", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ phrase: phraseInput }),
+                body: JSON.stringify({
+                    phrase: phraseInput,
+                    instruct: instructResult,
+                }),
                 // const response = await fetch("/api/generate", {
                 //   method: "POST",
                 //   headers: {
@@ -31,8 +55,9 @@ export default function Home() {
                 );
             }
 
-            setResult(data.result);
+            setEditResult(data.editResult);
             setPhraseInput("");
+            setPhraseButton(false);
         } catch (error) {
             // Consider implementing your own error handling logic here
             console.error(error);
@@ -49,8 +74,20 @@ export default function Home() {
 
             <main className={styles.main}>
                 <img src="/dog.png" className={styles.icon} />
+                <h3>How should I edit?</h3>
+                <form onSubmit={onSubmitInstruction}>
+                    <input
+                        type="text"
+                        name="instruct"
+                        placeholder="Enter your instruction for editing"
+                        value={instructInput}
+                        onChange={(e) => setInstructInput(e.target.value)}
+                    />
+                    <input type="submit" value="Confirm Instruction" />
+                </form>
+                <div className={styles.result}>{instructResult}</div>
                 <h3>Edit thy thought!</h3>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={onSubmitPhrase}>
                     <input
                         type="text"
                         name="phrase"
@@ -58,9 +95,13 @@ export default function Home() {
                         value={phraseInput}
                         onChange={(e) => setPhraseInput(e.target.value)}
                     />
-                    <input type="submit" value="Edit now" />
+                    <input
+                        type="submit"
+                        value="Edit now"
+                        disabled={phraseButton}
+                    />
                 </form>
-                <div className={styles.result}>{result}</div>
+                <div className={styles.result}>{editResult}</div>
             </main>
         </div>
     );
